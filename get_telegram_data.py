f1a1a2e3ca6d31ec16a672012e8bfa38af5751bc
Victor <victor.kryukov@gmail.com>
2020-11-21 01:57:14 -0800

    Extract get_messages and remove business logic from it

--- get_telegram_data.py
+++ get_telegram_data.py
@@ -21,42 +21,38 @@ class MoscowData:
         self.infected, self.hospitalized, self.ventilated = [int(s) for s in words if s.isdigit()]
 
 
+def get_messages(url, limit=100):
+    """Get LIMIT messages from Telegram channel with given URL."""
+    client = TelegramClient(username, api_id, api_hash)
+
+    async def get_channel_data():
+        nonlocal client
+        channel = await client.get_entity(url)
+        history = await client(GetHistoryRequest(
+            peer=channel,
+            offset_id=0,
+            offset_date=None,
+            add_offset=0,
+            limit=limit,
+            max_id=0,
+            min_id=0,
+            hash=0
+        ))
+        return history.messages
+
+    with client:
+        return client.loop.run_until_complete(get_channel_data())
 
 
-async def get_channel_data():
-    opershtab_channel = await client.get_entity(moscow_covid_channel)
-
-    database = []  # Сюда будем писать данные по датам
-
-    history = await client(GetHistoryRequest(
-        peer=opershtab_channel,
-        offset_id=0,
-        offset_date=None,
-        add_offset=0,
-        limit=100,
-        max_id=0,
-        min_id=0,
-        hash=0
-    ))
-    if not history.messages:
-        return "Нет сообщений в канале оперштаба!"
-
-    messages = history.messages
-    for message in messages:
+def get_opershtab_db():
+    database = []
+    for message in get_messages(moscow_covid_channel, 100):
         if '️В Москве за сутки госпитализировали' in message.raw_text:
             # Берём данные из третьей строки
             words = message.raw_text.splitlines()[2].split()
             # Вычленяем три числа из третьей строки
             infected, hospitalized, ventilated = [int(s) for s in words if s.isdigit()]
             database.append([message.date, infected, hospitalized, ventilated])
-
-    return database
-
-
-def get_opershtab_db():
-    # Запускаем в асинхронном режиме
-    with client:
-        database = client.loop.run_until_complete(get_channel_data())
     return database
 
 
