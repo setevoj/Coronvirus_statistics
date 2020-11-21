9607cf60f253c05793cb32797255b9bc3e94e59d
Victor <victor.kryukov@gmail.com>
2020-11-21 02:06:02 -0800

    Uppercase constant name

--- moscow_data.py
+++ moscow_data.py
@@ -1,7 +1,7 @@
 # Модуль для парсинга данных из телеграм-канала оперштаба Москвы
 from telegram import get_messages
 
-moscow_covid_channel = 'https://t.me/COVID2019_official'
+MOSCOW_COVID_CHANNEL = 'https://t.me/COVID2019_official'
 
 
 MOSCOW_DATA_TAG = '️В Москве за сутки госпитализировали'
@@ -19,7 +19,7 @@ class MoscowData:
 
 def get_opershtab_db():
     database = []
-    for message in get_messages(moscow_covid_channel, 100):
+    for message in get_messages(MOSCOW_COVID_CHANNEL, 100):
         if '️В Москве за сутки госпитализировали' in message.raw_text:
             # Берём данные из третьей строки
             words = message.raw_text.splitlines()[2].split()
