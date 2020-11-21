deddf072eee77102974df1192ac5061e42832e8e
Vladimir Kondratiev <vladimir.kondratiev@3keys.com>
2020-11-21 15:28:16 +0300

    fix comments, quotes, and spaces

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ import get_telegram_data
 
 
 def text2int(str_array):
-    # Конвертируем массив текстовых форматированных записей в массив чисел
+    # Конвертируем массив текстовых форматированных записей в массив чисел.
     return [int(s.replace(' ', '')) for s in str_array]
 
 
 def get_site_data():
-    # Получаем данные с сайта
+    # Получаем данные с сайта.
     browser = mechanicalsoup.StatefulBrowser()
     soup = BeautifulSoup(browser.open('https://xn--80aesfpebagmfblc0a.xn--p1ai/information/').text, features='lxml')
 
-    # Данные по Москве
-    ru_data = json.loads(soup.find("cv-stats-virus").attrs[":stats-data"])
+    # Данные по России.
+    ru_data = json.loads(soup.find('cv-stats-virus').attrs[':stats-data'])
     ru_sick, ru_sick_inc, ru_healed, ru_healed_inc, ru_died, ru_died_inc = text2int([ru_data['sick'],
                                                                                      ru_data['sickChange'],
                                                                                      ru_data['healed'],
@@ -29,8 +29,8 @@ def get_site_data():
     ru_inc_total_percentage = ru_sick_inc / (ru_sick - ru_sick_inc) * 100
     ru_inc_active_percentage = ru_sick_inc / (ru_active_yesterday) * 100
 
-    # Данные по региону
-    regions_data = soup.find("cv-spread-overview")  # and tag.has_attr('id') and tag['id'] == "Table1")
+    # Данные по Москве.
+    regions_data = soup.find('cv-spread-overview')  # and tag.has_attr('id') and tag['id'] == 'Table1')
     data = json.loads(regions_data.attrs[':spread-data'])
     mow_data = [x for x in data if x['title'] == 'Москва'][0]
     mow_sick, mow_sick_inc, mow_healed, mow_healed_inc, mow_died, mow_died_inc = [mow_data['sick'],
@@ -49,27 +49,27 @@ def get_site_data():
 
 
 def extract_last_data(db):
-    # Вычленяем данные оперштаба Москвы за последнюю доступную дату из общей базы
-    # Возвращаем данные за сегодняшнее число (или None, если таких данных не найдено), а также прирост/убыль
-    # госпитализаций и людей на аппаратах ИВЛ по сравнению с предыдущим днём
+    # Вычленяем данные оперштаба Москвы за последнюю доступную дату из общей базы.
+    # Возвращаем данные за сегодняшнее число (или None, если таких данных не найдено), а также прирост / убыль
+    # госпитализаций и людей на аппаратах ИВЛ по сравнению с предыдущим днём.
     if not db:
         return [None] * 5
 
-    # Данные отсортированы в обратном хронологическом порядке, поэтому первая запись самая актуальная
+    # Данные отсортированы в обратном хронологическом порядке, поэтому первая запись самая актуальная.
     last_available_record = db[0]
-    last_available_date = last_available_record[0].date() # Дата на первом месте, берём без времени
+    last_available_date = last_available_record[0].date()  # Дата на первом месте, берём без времени.
     hospitalized = last_available_record[2]
     ventilated = last_available_record[3]
 
-    # Пробуем получить данные за предыдущую дату
+    # Пробуем получить данные за предыдущую дату.
     previous_date = last_available_date - timedelta(days=1)
-    if len(db) > 1 and db[1][0].date() == previous_date: # Если есть данные за две даты и
-                                                        # если предыдущая дата была перед последней
+    if len(db) > 1 and db[1][0].date() == previous_date:  # Если есть данные за две даты и
+                                                          # если предыдущая дата была перед последней.
         previous_record = db[1]
     else:
         previous_record = []
 
-    if previous_record: # Есть данные за вчера
+    if previous_record:  # Есть данные за вчера.
         hosp_inc = hospitalized - previous_record[2]
         vent_inc = ventilated - previous_record[3]
     else:
@@ -81,62 +81,60 @@ def extract_last_data(db):
 def get_siteinfo_message(*args):
     ru_sick_inc, ru_inc_total_percentage, ru_inc_active_percentage, ru_sick, \
     mow_sick_inc, mow_inc_total_percentage, mow_inc_active_percentage, mow_sick = args
-    return  f"Россия: {ru_sick_inc:+d} человек ({ru_inc_total_percentage:+.2f}% от всех случаев, " \
-            f"{ru_inc_active_percentage:+.2f}% от активных случаев), всего {ru_sick:,}.\n" \
-            f"Москва: {mow_sick_inc:+d} человек (соответственно {mow_inc_total_percentage:+.2f}% , " \
-            f"{mow_inc_active_percentage:+.2f}%), всего {mow_sick:,}."
+    return f'Россия: {ru_sick_inc:+d} человек ({ru_inc_total_percentage:+.2f}% от всех случаев, ' \
+           f'{ru_inc_active_percentage:+.2f}% от активных случаев), всего {ru_sick:,}.\n' \
+           f'Москва: {mow_sick_inc:+d} человек (соответственно {mow_inc_total_percentage:+.2f}% , ' \
+           f'{mow_inc_active_percentage:+.2f}%), всего {mow_sick:,}.'
 
 
 def get_tginfo_message(*args):
     hospitalized, ventilated, hosp_inc, vent_inc, data_date = args
 
-    # Формируем правильное имя для дня по которому есть данные в телеграме
+    # Формируем правильное имя для дня, по которому есть данные в телеграме.
     if data_date is None:
         day_name = 'последнее время'
     elif data_date == datetime.today().date():
-        day_name = "вчера"
+        day_name = 'вчера'
     elif data_date == datetime.today().date() - timedelta(days=1):
-        day_name = "позавчера"
+        day_name = 'позавчера'
     else:
-        day_name = data_date.strftime("%m.%d.%Y")
+        day_name = data_date.strftime('%m.%d.%Y')
 
     if hospitalized is not None:
-        message = f"Число госпитализированных в Москве за {day_name}: {hospitalized}"
+        message = f'Число госпитализированных в Москве за {day_name}: {hospitalized}'
         if hosp_inc is not None:
-            message += f" ({hosp_inc:+d} к предыдущему дню).\n"
+            message += f' ({hosp_inc:+d} к предыдущему дню).\n'
         else:
-            message += "(данных за предыдущий день нет).\n"
-        message += f"Число больных на ИВЛ в Москве за {day_name}: {ventilated}"
+            message += '(данных за предыдущий день нет).\n'
+        message += f'Число больных на ИВЛ в Москве за {day_name}: {ventilated}'
         if vent_inc is not None:
-            message += f" ({vent_inc:+d} к предыдущему дню)."
+            message += f' ({vent_inc:+d} к предыдущему дню).'
         else:
-            message += "(данных за предыдущий день нет)."
+            message += '(данных за предыдущий день нет).'
     else:
-        message = f"Нет данных по госпитализациям в Москве за {day_name}."
-        message += f"\nНет данных по больным на ИВЛ в Москве за {day_name}."
+        message = f'Нет данных по госпитализациям в Москве за {day_name}.'
+        message += f'\nНет данных по больным на ИВЛ в Москве за {day_name}.'
 
     return message
 
 
 def print_data():
-    print("Запрашиваем данные с сайта...")
+    print('Запрашиваем данные с сайта...')
     site_data = get_site_data()
-    print("Данные с сайта получены.")
+    print('Данные с сайта получены.')
 
-    print("Запрашиваем данные из телеграма...")
+    print('Запрашиваем данные из телеграма...')
     db = get_telegram_data.get_opershtab_db()
-    # Берём данные за последний доступный день
+
+    # Берём данные за последний доступный день.
     tg_data = extract_last_data(db)
-    print("Данные из телеграма получены.\n")
+    print('Данные из телеграма получены.\n')
 
-    print("Сформированное сообщение о ситуации:\n====================================\n")
+    print('Сформированное сообщение о ситуации:\n====================================\n')
 
-    print("#коронавирус\n#официальныеданные\n#указаниясобянинавыполним\n")
+    print('#коронавирус\n#официальныеданные\n#указаниясобянинавыполним\n')
     print(get_siteinfo_message(*site_data) + '\n' + get_tginfo_message(*tg_data))
 
 
 if __name__ == '__main__':
     print_data()
-
-
-
